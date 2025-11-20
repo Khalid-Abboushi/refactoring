6970ddd5e648e9147e88764ebf19c95754657074
Khalid-Abboushi-uoft <Khalid.abboushi@mail.utoronto.ca>
2025-11-20 13:54:22 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,25 @@
 package theater;
 
+/**
+ * Represents a play with a name and type.
+ *
+ * @null no fields may be null
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
